2b9f7052fe98eb32ffd82474d14cccbae303870f
Christian <christian.nader@decathlon.com>
2022-06-29 10:40:59 +0200

    basic reactive server setup

--- store-app/src/main/java/com/decathlon/ContractFirstApplication.java
+++ store-app/src/main/java/com/decathlon/ContractFirstApplication.java
@@ -1,27 +1,14 @@
 package com.decathlon;
 
-import com.decathlon.bike.ApiClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class ContractFirstApplication {
 
     public static void main(String[] args) {
         new SpringApplication(ContractFirstApplication.class).run(args);
-    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
-    @Bean
-    public ApiClient apiClient() {
-        return new ApiClient().setBasePath("http://localhost:8080/bikes/v1");
     }
 
 }
--- store-app/src/main/java/com/decathlon/application/ItemsImpl.java
+++ store-app/src/main/java/com/decathlon/application/ItemsImpl.java
@@ -1,67 +1,26 @@
 package com.decathlon.application;
 
-import com.decathlon.bike.api.BikesApi;
-import com.decathlon.bike.model.Bike;
 import com.decathlon.store.api.ItemsApiDelegate;
 import com.decathlon.store.model.Item;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 import java.time.OffsetDateTime;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ItemsImpl implements ItemsApiDelegate {
 
-    private final BikesApi bikesApi;
-
     @Override
-    public ResponseEntity<Void> createItem(Item item) {
-        return new ResponseEntity<>(HttpStatus.valueOf(201));
-    }
-
-    @Override
-    public ResponseEntity<Void> deleteItem(String itemId) {
-        return new ResponseEntity<>(HttpStatus.valueOf(204));
-    }
-
-    @Override
-    public ResponseEntity<Item> getItem(String itemId) {
-        System.out.println("Hello from stores app!");
-        try {
-            Bike bike = bikesApi.showBikeById(itemId);
-            return ResponseEntity.status(HttpStatus.ACCEPTED)
-                    .body(
-                            Item.builder()
-                                    .lastUpdated(OffsetDateTime.now().toString())
-                                    .description("it's a " + bike.getColor() + " " + bike.getBrand())
-                                    .available(true)
-                                    .date(OffsetDateTime.now())
-                                    .name(bike.getName())
-                                    .build()
-                    );
-        } catch (Exception e) {
-            return ResponseEntity.notFound().build();
-        }
-    }
-
-    @Override
-    public ResponseEntity<List<Item>> getItems() {
-//        Item item = Item.builder()
-//                .lastUpdated(OffsetDateTime.now().toString())
-//                .description("it's shoes!")
-//                .available(true)
-//                .date(OffsetDateTime.now())
-//                .name("kiprun")
-//                .build();
-        return ResponseEntity.ok(List.of());
-    }
-
-    @Override
-    public ResponseEntity<Void> updateItem(String itemId, Item item) {
-        return new ResponseEntity<>(HttpStatus.valueOf(202));
+    public Mono<ResponseEntity<Item>> getItem(String itemId, ServerWebExchange exchange) {
+        return Mono.just(ResponseEntity.ok(Item.builder()
+                .color("red")
+                .date(OffsetDateTime.now())
+                .description("mono item")
+                .name("item")
+                .build()));
     }
 }
